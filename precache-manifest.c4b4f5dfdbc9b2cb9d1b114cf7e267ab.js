9e30ee1e55ac76e7f3e88f45f001bbb29c63e476
alrtn4 <45690266+alrtn4@users.noreply.github.com>
2020-07-22 07:27:07 +0430

    Updates

---
+++ precache-manifest.c4b4f5dfdbc9b2cb9d1b114cf7e267ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ba8f729820105f31488",
+    "url": "/maktabkhune-todo/static/css/main.1533ac2f.chunk.css"
+  },
+  {
+    "revision": "8ba8f729820105f31488",
+    "url": "/maktabkhune-todo/static/js/main.8ba8f729.chunk.js"
+  },
+  {
+    "revision": "0797db48e8d813587d0c",
+    "url": "/maktabkhune-todo/static/js/1.0797db48.chunk.js"
+  },
+  {
+    "revision": "edf92b1d4a3b055a9dbb",
+    "url": "/maktabkhune-todo/static/js/runtime~main.edf92b1d.js"
+  },
+  {
+    "revision": "2104b323b5a9d7081fd8fc6f15980052",
+    "url": "/maktabkhune-todo/index.html"
+  }
+];
